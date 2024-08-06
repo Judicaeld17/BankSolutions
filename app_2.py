00a6b257a5f3f367bf146d2b09bb43ad8357a920
Judicael Mahoutin DJIDONOU <117901002+Judicaeld17@users.noreply.github.com>
2024-08-06 19:56:36 +0100

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -50,26 +50,26 @@ if uploaded_file is not None:
 
     with col2:
         # Display the uploaded data
-        # st.write("Uploaded Data:")
-        # st.write(data)
+        st.write("Uploaded Data:")
+        st.write(data)
 
         # Slider to set the threshold
         threshold = st.slider("Set the threshold for decision-making", min_value=0.0, max_value=1.0, value=0.5, step=0.01)
 
         # Update the DataFrame with predictions based on the selected threshold
-        output_data['Prediction'] = ["Non approuvé" if score < threshold else "Approuvé" for score in prediction_scores]
+        output_data['Prediction'] = ["❌ Non approuvé" if score < threshold else "✅ Approuvé" for score in prediction_scores]
 
         # Display the updated predictions
         st.write("Updated Predictions with Threshold:")
         st.write(output_data)
 
         # Count the number of "Approuvé" and "Non approuvé"
-        count_approuve = output_data['Prediction'].value_counts().get("Approuvé", 0)
-        count_non_approuve = output_data['Prediction'].value_counts().get("Non approuvé", 0)
+        count_approuve = output_data['Prediction'].str.contains("✅").sum()
+        count_non_approuve = output_data['Prediction'].str.contains("❌").sum()
 
         # Display the counts
-        st.write(f"Total 'Approuvé': {count_approuve}")
-        st.write(f"Total 'Non approuvé': {count_non_approuve}")
+        st.write(f"Total '✅ Approuvé': {count_approuve}")
+        st.write(f"Total '❌ Non approuvé': {count_non_approuve}")
 
 else:
     with col2:
